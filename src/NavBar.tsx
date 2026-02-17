06c89d51ddaff9eabb08e1e6cd6280e14ce460ad
HarmanHans <harmanhans56@gmail.com>
2026-02-16 19:33:37 -0800

    Merge branch 'main' of https://github.com/HarmanHans/vct-grids

---
+++ src/NavBar.tsx
@@ -0,0 +1,9 @@
+function NavBar() {
+   return (
+    <nav className='navbar' role='navigation'>
+        <div className='navbar-name'>VCT Grids</div>
+    </nav>
+   ) 
+}
+
+export default NavBar;
